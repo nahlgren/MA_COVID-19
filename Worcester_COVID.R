2d65b943bf527754178d7af3de0bb26739d253dd
nahlgren <nahlgren@users.noreply.github.com>
2020-05-24 17:57:07 -0400

    Add files via upload
    Update Worcester city data.
    Modify Worcester_COVID.R to include hospitalizations (7 day mean), and add yellow points to all Worcester city plots.

--- Worcester_COVID.R
+++ Worcester_COVID.R
@@ -4,13 +4,17 @@ library(gridExtra)
 
 d<-read.table(file="Worcester_city.tsv",sep="\t",header=T)
 death.d<-read.table(file="Worcester_deaths.tsv",sep="\t",header=T)
+hosp.d<-read.table(file="Worcester_hospitalizations.tsv",sep="\t",header=T)
 
-# process dates, cases, deaths
+# process dates, cases, deaths, hospitalizations
 d$Date<-as.Date(d$Date)
 d$cases<-as.numeric(d$cases)
 
 death.d$Date<-as.Date(death.d$Date)
 death.d$Deaths<-as.numeric(death.d$Deaths)
+
+hosp.d$Date<-as.Date(hosp.d$Date)
+hosp.d$Hospitalizations<-as.numeric(hosp.d$Hospitalizations)
 
 
 ### calculate new daily cases
@@ -19,6 +23,10 @@ d$dnew<-c(0,d$cases[c(2:length(d$cases))] - d$cases[c(1:(length(d$cases)-1))])
 
 ### calculate new daily deaths
 death.d$dnew<-c(0,death.d$Deaths[c(2:length(death.d$Deaths))] - death.d$Deaths[c(1:(length(death.d$Deaths)-1))])
+
+### calculate new daily hospitalizations
+hosp.d$dnew<-c(0,hosp.d$Hospitalizations[c(2:length(hosp.d$Hospitalizations))] - hosp.d$Hospitalizations[c(1:(length(hosp.d$Hospitalizations)-1))])
+
 
 
 ## Running Days
@@ -39,6 +47,19 @@ for (i in c(7:length(death.d$Deaths)) ) {
 	weekly.mean<-c(weekly.mean,mean(death.d $dnew[c((i-6):i)]))
 }
 death.d$dnew.weekmean<-weekly.mean
+
+weekly.mean<-rep(NA,6)
+for (i in c(7:length(hosp.d$Hospitalizations)) ) {
+	weekly.mean<-c(weekly.mean,mean(hosp.d$dnew[c((i-6):i)]))
+}
+hosp.d$dnew.weekmean<-weekly.mean
+
+weekly.mean<-rep(NA,6)
+for (i in c(7:length(hosp.d$Hospitalizations)) ) {
+	weekly.mean<-c(weekly.mean,mean(hosp.d$Hospitalizations[c((i-6):i)]))
+}
+hosp.d$hosp.weekmean<-weekly.mean
+
 
 ########## Determine linear regressions on case data last 7 or 14 days
 ########## and extraoplate in the future for 14 days
@@ -94,8 +115,21 @@ d.legend.2$dnew<-0.9*c(max(d$dnew),max(d$dnew))
 font.size=14
 	
 sun.start<-as.Date("2020/03/15")
-suns<-c(2+seq(from=0,by=7,dim(d)[1]))
+seq.c<-seq(from=0,by=7,to=as.numeric(as.Date("2020-05-24")-sun.start)/7)
+#suns<-c(2+seq(from=0,by=7,dim(d)[1]))
+suns.seq<-sun.start+seq.c
+
+suns<-c()
+for (i in c(1:length(suns.seq))) {
+	sun.i<-which(d$Date==suns.seq[i])
+	if (length(which(d$Date==suns.seq[i]))==0) {
+	} else {
+		suns<-c(suns,which(d$Date==suns.seq[i]))	
+		}
+	}
+
 d.suns<-d[suns,]
+deaths.d.suns<-death.d[suns,]
 
 ####### Create ggplots to arrange together 
 
@@ -185,23 +219,37 @@ p6 <- ggplot(data = d, aes(x = Date, y = dnew.weekmean)) +
 
 
 ##### Date vs cases & deaths linear 
-coeff<-max(d$dnew.weekmean,na.rm=T)/max(death.d$dnew.weekmean,na.rm=T)
+death.coeff<-max(d$dnew.weekmean,na.rm=T)/max(death.d$dnew.weekmean,na.rm=T)
 p7 <- ggplot(data = d, aes(x = Date)) +
 		geom_point(aes(y=dnew.weekmean), cex=3) +
-		geom_point(data = death.d, aes(y=dnew.weekmean*coeff),col='red',cex=3) +
 		geom_line(aes(y=dnew.weekmean)) +
-		geom_line(data = death.d, aes(y=dnew.weekmean*coeff),col='red') +
-		scale_y_continuous(sec.axis = sec_axis(~./coeff, name = "Daily new deaths")) + 
+        geom_point(data = d.suns, aes(x = Date, y = dnew.weekmean), bg='yellow', col='black',cex=3, pch=21 ) +
+		geom_line(data = death.d, aes(y=dnew.weekmean*death.coeff),col='red') +
+		geom_point(data = death.d, aes(y=dnew.weekmean*death.coeff),col='red',cex=3) +
+		scale_y_continuous(sec.axis = sec_axis(~./death.coeff, name = "Daily new deaths")) + 
 #      geom_line(cex=2) +
  #     scale_y_log10() +
-    theme(plot.title = element_text(hjust = 0.5), text=element_text(size=font.size)) +
-      labs(title = "Worcester daily new COVID-19 cases and deaths, 7-day mean",
+    theme(plot.title = element_text(hjust = 0.5), text=element_text(size=font.size), axis.title.y.right = element_text(color = 'red')) +
+      labs(title = "Worcester daily new COVID-19 cases\n and deaths, 7 day mean",
            x = "Date", y = "Daily new cases")
 
-
-
 #plot(d$Date,d$cases)
-#points(death.d$Date,death.d$Deaths*coeff,col='red')
+#points(death.d$Date,death.d$Deaths*death.coeff,col='red')
+
+hosp.coeff<-max(d$dnew.weekmean,na.rm=T)/max(hosp.d$hosp.weekmean,na.rm=T)
+p8 <- ggplot(data = d, aes(x = Date)) +
+		geom_point(aes(y=dnew.weekmean), cex=3) +
+		geom_line(aes(y=dnew.weekmean)) +
+        geom_point(data = d.suns, aes(x = Date, y = dnew.weekmean), bg='yellow', col='black',cex=3, pch=21 ) +	
+        geom_point(data = hosp.d, aes(y=hosp.weekmean*hosp.coeff),col='red',cex=3) +
+		geom_line(data = hosp.d, aes(y=hosp.weekmean*hosp.coeff),col='red') +
+		scale_y_continuous(sec.axis = sec_axis(~./hosp.coeff, name = "Hospitilizations")) + 
+#      geom_line(cex=2) +
+ #     scale_y_log10() +
+    theme(plot.title = element_text(hjust = 0.5), text=element_text(size=font.size), axis.title.y.right = element_text(color = 'red')) +
+      labs(title = "Worcester daily new COVID-19 cases\nand hospitalizations, 7 day mean",
+           x = "Date", y = "Daily new cases")
+
 
 
 t<-textGrob(expression(bold("COVID-19 case data for Worcester, MA")),gp=gpar(fontsize=28))
@@ -222,7 +270,11 @@ jpeg("Worcester_COVID_v2.jpg",width=800,height=800)
 grid.arrange(p1,p2,p3,p4,nrow=2,top=t,bottom=t2)
 dev.off()
 
-jpeg("Worcester_COVID_cases_deaths.jpg",width=400,height=400)
+jpeg("Worcester_COVID_cases_deaths.jpg",width=600,height=400)
 grid.arrange(p7,nrow=1)
+dev.off()
+
+jpeg("Worcester_COVID_7daymean_cases_hospitalizations.jpg",width=600,height=400)
+grid.arrange(p8,nrow=1)
 dev.off()
 
